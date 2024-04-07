c5eecd471d3396087064f066fbc6f588c126713a
Mohit Varshney <83027288+mohitvarshney07@users.noreply.github.com>
2024-04-07 19:24:23 +0000

    done

---
+++ 06_high_order_array_loops/for_each.js
@@ -0,0 +1,21 @@
+// let arr = ['c++','java','ruby','python','javascript']
+// arr.forEach((val) =>{
+//     console.log(val)
+// })
+
+let num = [1,2,3,4,5,6,7,8,9,10]
+// let new_num = num.filter((n)=> n>4)
+// let new_num = num.filter((n)=>{
+//     return n>5
+// })
+// console.log(new_num)
+
+// let newnum =[]
+// num.forEach((n)=>{
+//     if(n>4){
+//         newnum.push(n)
+//     }
+// })
+// console.log(newnum)
+
+
